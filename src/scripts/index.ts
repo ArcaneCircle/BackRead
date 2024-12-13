65225127ec6bb91d11a042e4b2a9d1d3951f5a89
adbenitez <asieldbenitez@gmail.com>
2024-12-13 02:04:54 +0100

    apply prettier

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,15 +1,17 @@
 import "../styles/index.scss";
-import "@webxdc/highscores"
+import "@webxdc/highscores";
 import { GameListeners } from "./class/GameListeners";
 import { GameMenu } from "./class/GameMenu";
 import { injector } from "./const/injector";
 
 const scoreboard = document.getElementById("scoreboard");
-window.highscores.init({
-  onHighscoresChanged: () => {
-    scoreboard.innerHTML = window.highscores.renderScoreboard().innerHTML;
-  },
-}).then(() => {
-  injector.injectClass(GameMenu);
-  injector.injectClass(GameListeners);
-});
+window.highscores
+  .init({
+    onHighscoresChanged: () => {
+      scoreboard.innerHTML = window.highscores.renderScoreboard().innerHTML;
+    },
+  })
+  .then(() => {
+    injector.injectClass(GameMenu);
+    injector.injectClass(GameListeners);
+  });
